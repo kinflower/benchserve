11373ad0a3d5f698484a37dc97a8b5cfe3b1cdd8
aliyun6345328460 <1415009447@qq.com>
2024-10-24 15:54:50 +0800

    style：移除未使用的代码

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ const path = require('path')
 
 // 网址可直接访问public下的资源
 app.use(express.static(path.join(__dirname, 'public')))
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 personal.Personal({router, url, db: db.personal})
 
